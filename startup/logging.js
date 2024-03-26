a2495c4ab04c6e0f54e5713bca30c264a928043c
Kinsammy <fanusamuel@gmail.com>
2024-03-26 15:25:48 +0100

    Logging in  progress...

--- startup/logging.js
+++ startup/logging.js
@@ -1,3 +1,29 @@
 import winston from "winston";
 import "winston-mongodb";
 import "express-async-errors";
+import config from "config";
+
+module.exports = () => {
+  winston.exceptions.handle(
+    new winston.transports.File({
+      filename: "uncaughtExceptions.log",
+    })
+  );
+
+  process.on("unhandledRejection", (exceptions) => {
+    throw exceptions;
+  });
+
+  winston.add(
+    new winston.transports.File({
+      filename: "logfile.log",
+    })
+  );
+
+  winston.add(
+    new winston.transports.MongoDB({
+      db: config.get("db_url"),
+      level: "info",
+    })
+  );
+};
