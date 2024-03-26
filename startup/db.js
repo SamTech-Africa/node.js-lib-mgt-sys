3c14fc8b220ae3ca3b3d6f588cee9ddb254a1a96
Kinsammy <fanusamuel@gmail.com>
2024-03-26 16:00:31 +0100

    Logging in  progress...

--- startup/db.js
+++ startup/db.js
@@ -4,9 +4,12 @@ import winston from "winston";
 
 const db = () => {
   mongoose
-    .connect(config.get("db_url"))
+    .connect(config.get("db_url"), {
+      autoIndex: false,
+    })
     .then(() => winston.info("Database is connected successfully"))
     .catch((error) => winston.error("Error connecting to database.", error));
+  process.exit(-1);
 };
 
 export default db;
